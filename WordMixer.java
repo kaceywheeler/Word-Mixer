3dfaed74a5f391ca61b47ffcdb18c5b2e37f013d
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 14:01:23 -0700

    Update WordMixer.java

--- WordMixer.java
+++ WordMixer.java
@@ -3,19 +3,26 @@ package ifs2;
 public class WordMixer {
 	public String Mix(String text, int num) {
 		String answer;
+		//Save length of string as variable
 		int len = text.length();
 		
+		//Makes sure that the index trying to be used is not bigger than the length of the text
 		if (num < len) {
+			//Creates substring from the beginning to the number
 			String sub1 = text.substring(0,num);
+			//Creates substring of remaining characters
 			String sub2 = text.substring(num, len);
+			//Adds substrings together to make new string in reverse order
 			answer = sub2 + sub1;
 		
 		}
 		
+		//Prints no can do if the number is outside of the length of the string
 		else {
 			answer = "no can do";
 		}
 		
+		//Return new string 
 		return answer;
 	}
 }
